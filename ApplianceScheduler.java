3ff1c4f0cfdca5237b47c77804670c9e581dc951
HannahCovington <hcovington@scu.edu>
2021-11-23 08:49:08 -0800

    Update ApplianceScheduler.java

--- ApplianceScheduler.java
+++ ApplianceScheduler.java
@@ -8,41 +8,79 @@ public class ApplianceScheduler {
 
 	public static void main(String[] args) throws IOException {
 		
-		//DECLARING VARIABLES--------------------
-		//dishwasher use
-		double dishwasherNotPresentProb = .5120;
-		double sit1prob = .327; //zero runs 
-		double sit2prob = .188 + sit1prob; //one run
-		double sit3prob = .278 + sit2prob; //2 to 3 runs
-		double sit4prob = .131 + sit3prob; //4 to 6 runs
-		double sit5prob = .082 + sit4prob; //7+ runs
+		//INPUTS=======================================
+		double dishwasherNotPresentProb = .5120; //probability of there not being a dishwasher in the home
+		double sit1prob = .327; //probability for zero runs 
+		double sit2prob = .188 + sit1prob; //probability for one run
+		double sit3prob = .278 + sit2prob; //probability for 2 to 3 runs
+		double sit4prob = .131 + sit3prob; //probability for 4 to 6 runs
+		int timeStepInMinutes = 60; //time step length in minutes !!!MAKE EASILY DIVISIBLE INTO 60!!!
 		int duration = 2; //number of timesteps the dishwasher is run 
-		int timeStepInMinutes = 60; //time step length in minutes MAKE EASILY DIVISIBLE INTO 60!!!
-		int timeStep = 60/timeStepInMinutes; //time step length in minutes NEEDS TO BE WHOLE NUMBER!!!
 		boolean schedulingWeek = true; //will we be scheduling entire weeks?
-		//random numbers that determine use
+		// ===============================================
+
+		//DECLARING VARIABLES ===============================
 		double determinePresenceOfDishwasher;
 		double weekClassification;
 		double determineLoadsThisWeek;
 		double determineTimeStep;
-		//variables for holding information
 		int loadsThisWeek = 0;
-		int numOperationalHours;
-		int operationSchedule[];
 		boolean dishwasherPresent;
-		int occupied=0;
+		int occupied = 0;
 		double probNew = 0;
 		double probOld;
 		boolean trip = false;
+		int timeStep = 60/timeStepInMinutes; //time step length in minutes !!!NEEDS TO BE WHOLE NUMBER!!!
 		ArrayList<Integer> activeTimeSteps = new ArrayList<Integer>();
 		ArrayList<Integer> turnOnTimeSteps = new ArrayList<Integer>();
 		ArrayList<Integer> occupancyData = new ArrayList<Integer>();
 		ArrayList<Integer> dailySchedule = new ArrayList<Integer>();
-		int timeStepCount = 0;
-
-		//----------------------------------------------------
+		int dayCount=0;
+		int Ncount=3;
+		FileWriter dailyWriter = new FileWriter("C:\\Users\\Hannah\\Desktop\\DailySchedules.csv");
+		//=================================================
 		
-		//DISHWASHER PRESENCE IN HOUSEHOLD ------------
+		//CHECK VALIDITY OF TIME STEP======================
+		if (60%timeStepInMinutes != 0){
+			System.out.println("=========================================================");
+			System.out.println("ERROR: Occupancy data does not represent a whole number of days.");
+			System.out.println("Results will not be valid.");
+			System.out.println("=========================================================");
+		}
+		//=================================================
+
+		//WRITING INITIAL FILES============================
+		dailyWriter.append("============================\n");
+		dailyWriter.append("Paste the Below Schedules Into the\n 'SCHEDULE:DAY:LIST' \nSection in IDF File\n");
+		dailyWriter.append("============================\n\n");
+
+		//GET OCCUPANCY DATA ==============================
+			//reading file
+			File data = new File("C:\\Users\\Hannah\\Desktop\\Occupancy.csv");
+			Scanner scanner = new Scanner(data);
+			scanner.useDelimiter(",");
+			while (scanner.hasNext()) {
+				occupancyData.add(scanner.nextInt());
+			}
+			scanner.close();
+			System.out.println("OCCUPANCY:");
+			System.out.println(occupancyData);
+			System.out.println("size: " + occupancyData.size());
+			//making sure it is a proper length
+			if (occupancyData.size()%(24*timeStep) != 0){
+				System.out.println("=========================================================");
+				System.out.println("ERROR: Occupancy data does not represent a whole number of days.");
+				System.out.println("=========================================================");
+			}
+			//how many time steps are occupied? (add awake later)
+			for (int i = 0; i<occupancyData.size();i++) {
+				if (occupancyData.get(i)==1) {
+					occupied++;	
+				}
+			}
+		//======================================================
+
+		//DISHWASHER PRESENCE IN HOUSEHOLD=================
 		determinePresenceOfDishwasher = Math.random();
 		if (determinePresenceOfDishwasher <= dishwasherNotPresentProb) {
 			dishwasherPresent = false;
@@ -52,9 +90,9 @@ public class ApplianceScheduler {
 		}
 		System.out.println("dishwasher presence");
 		System.out.println(dishwasherPresent);
-		//------------------------------------
+		//=========================================================
 
-		//NUMBER OF DISHWASHER LOADS THIS WEEK---------------
+		//NUMBER OF DISHWASHER LOADS THIS WEEK======================
 		if (dishwasherPresent == true) {
 			weekClassification = Math.random();
 			if (weekClassification<sit1prob) {
@@ -83,48 +121,20 @@ public class ApplianceScheduler {
 		}
 		System.out.println("loads");
 		System.out.println(loadsThisWeek);
-		//----------------------------------------------------------
+		//===================================================
 		
-		//GETTING OCCUPANCY DATA ---------------------
-		if (dishwasherPresent = true){
-			//reading file
-			File data = new File("C:\\Users\\Hannah\\Desktop\\Occupancy.csv");
-			Scanner scanner = new Scanner(data);
-			scanner.useDelimiter(",");
-			while (scanner.hasNext()) {
-				occupancyData.add(scanner.nextInt());
-			}
-			scanner.close();
-			System.out.println("OCCUPANCY:");
-			System.out.println(occupancyData);
-			System.out.println("size: " + occupancyData.size());
-			//making sure it is a proper length
-			if (occupancyData.size()%(24*timeStep) != 0){
-				System.out.println("=========================================================");
-				System.out.println("ERROR: Occupancy data does not represent a whole number of days.");
-				System.out.println("=========================================================");
-			}
-			//how many time steps are occupied? (add awake later)
-			for (int i = 0; i<occupancyData.size();i++) {
-				if (occupancyData.get(i)==1) {
-					occupied++;	
-				}
-			}
-		}
-		//------------------------------------------------------------
-		
-		//DISTRIBUTING PROBABILITY FOR EACH TIMESTEP-------------------
-		double timeStepProb = 1d/occupied; //for now, everything has same probability
-		//------------------------------------------------------------
-		dishwasherPresent=true;
-		//SCHEDULING THE APPLIANCE--------------------------------------
+		//DISTRIBUTING PROBABILITY FOR EACH TIMESTEP==================
+		double timeStepProb = 1d/occupied; //for now, everything has same probability CHANGE TO POSSIBLE COUNT LATER ON
+		//============================================================
+	
+		//SCHEDULING THE APPLIANCE====================================
 		if (dishwasherPresent == true) {
 			//getting which occupied timesteps are activated
 			int j = 1;
 			int safeGuard = 0;
-			while(j<=loadsThisWeek) { 
+			while(j<=loadsThisWeek) { //go until number of loads reached 
 				safeGuard++;
-				if (safeGuard == 100000000) { //prevents infinite loop in case that loading not possible.
+				if (safeGuard == 100000000) { //prevents infinite loop in case loading not possible.
 					System.out.println("=========================================================");
 					System.out.println("ERROR: SafeGuard Reached. Schedule output will not be correct. Ensure that loading is possible.");
 					System.out.println("Safeguard may be erroneously reached due to high frequency of appliance operation");
@@ -133,33 +143,33 @@ public class ApplianceScheduler {
 					System.out.println("=========================================================");
 					break;
 				}
-				determineTimeStep = Math.random();
+				determineTimeStep = Math.random(); //roll random number for this load
 				for (int i = 0; i < occupied; i++) {
 					probOld = probNew;
 					probNew = probOld + timeStepProb;
-					if (determineTimeStep > probOld && determineTimeStep <= probNew) {
+					if (determineTimeStep > probOld && determineTimeStep <= probNew) { // when the load random number lands within time step probability
 						for (int k=i;k<=i+duration-1;k++) {
 							if(activeTimeSteps.contains(k)==true || k>=occupancyData.size() ) { //ensuring loads don't overlap or get cutoff by schedule length
-								trip = true;
+								trip = true; //re roll to avoid overlap or cutoff
 							}
 						}
 						if (trip==false) {
-							for (int m = i; m<=i+duration-1;m++) {
+							for (int m = i; m<=i+duration-1;m++) { //if we can schedule here, schedule the entire duration
 								activeTimeSteps.add(m);
 								turnOnTimeSteps.add(i);
 							}
 							j++;
 						}
-						trip = false;
+						trip = false; //reset trip boolean for next load
 					}
 				}
-			probNew = 0;
+			probNew = 0; //reset probability counter for next load
 			}	
 			System.out.println("Active Time Steps:");
 			System.out.println(activeTimeSteps);
 			//finalizing schedule
 			int[] applianceSchedule = new int[occupancyData.size()];
-			int occupiedCount=0;
+			int occupiedCount=0; //CHANGE TO POSSIBLE COUNT LATER ON
 			for(int i = 0; i < occupancyData.size();i++) {
 				if (applianceSchedule[i] == 1) {
 					if(occupancyData.get(i) == 1) {
@@ -181,12 +191,6 @@ public class ApplianceScheduler {
 			//================================================
 
 			//PARSE SCHEDULE INTO DAYS AND WRITE TO CSV ==============================
-			int dayCount = 0;
-			int Ncount = 3;
-			FileWriter dailyWriter = new FileWriter("C:\\Users\\Hannah\\Desktop\\DailySchedules.csv");
-			dailyWriter.append("============================\n");
-			dailyWriter.append("Paste the Below Schedules Into the\n 'SCHEDULE:DAY:LIST' \nSection in IDF File\n");
-			dailyWriter.append("============================\n\n");
 			for (int r = 0; r < applianceSchedule.length;r++){
 				dailySchedule.add(applianceSchedule[r]);
 				if (dailySchedule.size()==24*timeStep){
@@ -205,13 +209,10 @@ public class ApplianceScheduler {
 					dailySchedule.clear();
 					Ncount = 3;
 				}
-
-
 			}
-			dailyWriter.close();
 			//==================================================
 
-			//WRITE TO WEEKLY SCHEDULE CSV==============================
+			//WRITE TO WEEKLY SCHEDULE CSV============================== move to after everything
 			schedulingWeek = true;
 			dayCount = 1;
 			int weekCount = 1; //CHANGE LATER
@@ -226,13 +227,14 @@ public class ApplianceScheduler {
 					weeklyWriter.append("\tDay_"+dayCount+",			!- Day "+dayCount+" Schedule:Day Name \n");
 					dayCount++;
 				}
-				weeklyWriter.close();
 				System.out.println("WEEKLY WRITTEN");
+				weeklyWriter.close();
 			}
 			//====================================================
 
 		}
-		
+		dailyWriter.close();
+
 		//------------------------------------------------------------------
 		
 	}
